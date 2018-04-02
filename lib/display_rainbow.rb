c497bd422b983e58f7d842ba4428ac13fbe476d1
Mike C <mikecronin92@gmail.com>
2018-04-02 02:28:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
+def display_rainbow(clrs)
   msg = []
-  7.times {|c| msg << "#{colors[c][0].upcase}: #{colors[c]}"}
+  7.times {|c| msg << "#{clrs[c][0].upcase}: #{clrs[c]}"}
   puts msg.join(", ") + "\n"
 end
