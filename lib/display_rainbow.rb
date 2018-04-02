89e75080c15f3d768b50e74b47061ccc5f19d850
Mike C <mikecronin92@gmail.com>
2018-04-02 02:24:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   7.times do |color|
-    msg += "#{colors[color][0].capitalize}: #{colors[color]}, "
+    msg += "#{colors[color][0].upcase}: #{colors[color]}, "
   end
 end
